b5516fc05193641b6f058ab973dd77fed65cdb63
Frank Jakop <frank.jakop@tolina.de>
2012-05-31 09:02:45 +0200

    tests written

---
+++ src/test/java/de/jakop/ngcalsync/google/GoogleCalendarDAOTest.java
@@ -0,0 +1,16 @@
+package de.jakop.ngcalsync.google;
+
+import org.junit.Test;
+
+
+public class GoogleCalendarDAOTest {
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testX() throws Exception {
+
+	}
+}
