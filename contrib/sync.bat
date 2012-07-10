dbb5d8a9eb64bdb0a55a4d260458a9068418aa4d
Frank Jakop <frank.jakop@tolina.de>
2012-07-10 12:08:22 +0200

    starter for windows

--- contrib/sync.bat
+++ contrib/sync.bat
@@ -6,5 +6,5 @@ FOR /F "tokens=*" %%i in ('type %HOMEPATH%\.ngcalsync\env.properties') do SET %%
 set PATH=%NOTES_HOME%;%PATH%
 set NOTES_JAR=%NOTES_HOME%\jvm\lib\ext\Notes.jar
 
-java -cp ngcalsync.jar;%NOTES_JAR% de.jakop.ngcalsync.StartApplication
+start javaw -cp ngcalsync.jar;%NOTES_JAR% de.jakop.ngcalsync.StartApplication
 endlocal
