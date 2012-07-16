f29d2dee858b0360dc883c30b24f33f3fd15d7f4
Frank Jakop <frank.jakop@tolina.de>
2012-07-16 08:53:17 +0200

    implemented stateful tray icon

--- src/main/java/de/jakop/ngcalsync/TrayStarter.java
+++ src/main/java/de/jakop/ngcalsync/TrayStarter.java
@@ -1,20 +1,17 @@
 package de.jakop.ngcalsync;
 
 import java.awt.AWTException;
-import java.awt.Image;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
-import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.net.URL;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
@@ -26,6 +23,8 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
+import de.jakop.ngcalsync.util.StatefulTrayIcon;
+import de.jakop.ngcalsync.util.StatefulTrayIcon.State;
 import de.jakop.ngcalsync.util.logging.Log4JSwingAppender;
 
 /**
@@ -38,6 +37,7 @@ import de.jakop.ngcalsync.util.logging.Log4JSwingAppender;
 public class TrayStarter implements IApplicationStarter {
 
 	private final Log log = LogFactory.getLog(getClass());
+	private StatefulTrayIcon icon;
 
 	@Override
 	public void startApplication(final Application application) {
@@ -62,10 +62,13 @@ public class TrayStarter implements IApplicationStarter {
 
 		final SystemTray tray = SystemTray.getSystemTray();
 
-		final Image image = chooseIcon(tray);
-		final TrayIcon trayIcon = new TrayIcon(image, Constants.APPLICATION_NAME);
+		try {
+			icon = new StatefulTrayIcon();
+			icon.setState(State.NORMAL);
+		} catch (final IOException e) {
+			log.error("TrayIcon could not be loaded.", e);
+		}
 		final PopupMenu popup = new PopupMenu();
-		trayIcon.setImageAutoSize(true);
 
 		// FIXME i18n
 		// Create a pop-up menu components
@@ -82,10 +85,10 @@ public class TrayStarter implements IApplicationStarter {
 		//		popup.add(aboutItem);
 		popup.add(exitItem);
 
-		trayIcon.setPopupMenu(popup);
+		icon.setPopupMenu(popup);
 
 		try {
-			tray.add(trayIcon);
+			tray.add(icon);
 		} catch (final AWTException e) {
 			log.error("TrayIcon could not be added.", e);
 		}
@@ -96,12 +99,12 @@ public class TrayStarter implements IApplicationStarter {
 			@Override
 			public void actionPerformed(final ActionEvent e) {
 				final ExecutorService executor = Executors.newSingleThreadExecutor();
-				executor.submit(new SynchronizeCallable(application, tray));
+				executor.submit(new SynchronizeCallable(application));
 			}
 		};
 
 		syncItem.addActionListener(syncActionListener);
-		trayIcon.addActionListener(syncActionListener);
+		icon.addActionListener(syncActionListener);
 
 		logItem.addActionListener(new ActionListener() {
 
@@ -123,33 +126,13 @@ public class TrayStarter implements IApplicationStarter {
 		});
 	}
 
-	private Image chooseIcon(final SystemTray tray) {
-		// must be descending!
-		final int[] availableSizes = new int[] { 76, 48, 32, 24, 20, 16 };
-		final int size = (int) tray.getTrayIconSize().getWidth();
-		int leastDifference = 100;
-		for (final int availableSize : availableSizes) {
-			final int difference = availableSize - size;
-			if (difference < leastDifference && difference >= 0) {
-				leastDifference = difference;
-			}
-		}
-
-		final String chosenSize = String.valueOf(size + leastDifference);
-
-		final URL imageURL = getClass().getResource(String.format("/images/icon%sx%s.png", chosenSize, chosenSize));
-		final Image image = new ImageIcon(imageURL).getImage();
-		return image;
-	}
 
 	private class SynchronizeCallable implements Callable<Void> {
 
 		private final Application application;
-		private final SystemTray tray;
 
-		public SynchronizeCallable(final Application application, final SystemTray tray) {
+		public SynchronizeCallable(final Application application) {
 			this.application = application;
-			this.tray = tray;
 		}
 
 		@Override
@@ -160,12 +143,9 @@ public class TrayStarter implements IApplicationStarter {
 					JOptionPane.showMessageDialog(null, String.format("The configuration was upgraded, please check and restart synchronisation."));
 					return null;
 				}
-				final TrayIconRunnable trayIconRunnable = new TrayIconRunnable(tray);
-				final Thread trayIconThread = new Thread(trayIconRunnable);
-				trayIconThread.start();
+				icon.setState(State.BLINK);
 				application.synchronize();
-				trayIconRunnable.finish();
-				trayIconThread.join();
+				icon.setState(State.NORMAL);
 			} catch (final Exception ex) {
 				log.error(ExceptionUtils.getStackTrace(ex));
 				final String home = System.getenv("user.home");
@@ -177,42 +157,5 @@ public class TrayStarter implements IApplicationStarter {
 		}
 	}
 
-	private class TrayIconRunnable implements Runnable {
 
-		private boolean finish = false;
-		private final SystemTray tray;
-
-		/**
-		 * 
-		 * @param tray
-		 */
-		public TrayIconRunnable(final SystemTray tray) {
-			this.tray = tray;
-		}
-
-		@Override
-		public void run() {
-			final TrayIcon[] trayIcons = tray.getTrayIcons();
-			while (!finish) {
-				try {
-
-					for (final TrayIcon trayIcon : trayIcons) {
-						tray.remove(trayIcon);
-					}
-					Thread.sleep(500);
-
-					for (final TrayIcon trayIcon : trayIcons) {
-						tray.add(trayIcon);
-					}
-					Thread.sleep(500);
-				} catch (final Exception e) {
-					// does not really matter
-				}
-			}
-		}
-
-		public void finish() {
-			finish = true;
-		}
-	}
 }
---
+++ src/main/java/de/jakop/ngcalsync/util/StatefulTrayIcon.java
@@ -0,0 +1,168 @@
+package de.jakop.ngcalsync.util;
+
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.SwingUtilities;
+
+import de.jakop.ngcalsync.Constants;
+
+/**
+ * Represent system tray icon with multiple states, which are e.g. represented by blinking the icon.
+ */
+
+public class StatefulTrayIcon extends TrayIcon {
+
+	/** blinking interval in milliseconds */
+	private static final long BLINK_INTERVAL_MILLISECONDS = 100L;
+
+	/** Available widths and heights of image icons (always squares) */
+	private static int[] AVAILABLE_IMAGE_SIZES = new int[] { 76, 48, 32, 24, 20, 16 };
+
+	private final Image iconNormal;
+	private final Image iconWorking;
+
+	private Thread stateThread;
+	private AbstractFinishableRunnable stateRunnable;
+
+
+	/**
+	* States of icon.
+	*/
+	public enum State {
+		/** normal state, i.e. not working */
+		NORMAL,
+		/** blinking state (working) */
+		BLINK;
+	}
+
+	/**
+	* Constructor.
+	* 
+	* @throws java.io.IOException if an I/O error occurs while reading the icon image resources
+	*/
+	public StatefulTrayIcon() throws IOException {
+		super(new BufferedImage(1, 1, BufferedImage.TYPE_3BYTE_BGR));
+		setToolTip(Constants.APPLICATION_NAME);
+		setImageAutoSize(true);
+
+		final int size = chooseImageSize();
+
+		iconNormal = ImageIO.read(getClass().getResource(String.format("/images/tray/normal/icon_normal_%s.png", String.valueOf(size))));
+		iconWorking = ImageIO.read(getClass().getResource(String.format("/images/tray/working/icon_working_%s.png", String.valueOf(size))));
+
+	}
+
+	/**
+	 * Sets the tray icon to the given state. Waits for the previous state to terminate.
+	 * 
+	 * @param state
+	 */
+	public void setState(final State state) {
+
+		if (stateRunnable != null) {
+			stateRunnable.finish();
+		}
+
+		if (state == State.BLINK) {
+			stateRunnable = new BlinkingRunnable();
+		} else if (state == State.NORMAL) {
+			stateRunnable = new NormalRunnable();
+		} else {
+			throw new IllegalStateException(String.format("State %s is not supported.", state));
+		}
+
+		stateThread = new Thread(stateRunnable);
+		stateThread.start();
+
+	}
+
+	private int chooseImageSize() {
+		final int size = (int) SystemTray.getSystemTray().getTrayIconSize().getWidth();
+		int leastDifference = 100;
+		for (final int availableSize : AVAILABLE_IMAGE_SIZES) {
+			final int difference = availableSize - size;
+			if (difference < leastDifference && difference >= 0) {
+				leastDifference = difference;
+			}
+		}
+
+		return size + leastDifference;
+	}
+	private abstract class AbstractFinishableRunnable implements Runnable {
+
+		private boolean finish = false;
+
+		protected final boolean isFinished() {
+			return finish;
+		}
+
+		public final void finish() {
+			finish = true;
+		}
+	}
+
+	private class NormalRunnable extends AbstractFinishableRunnable {
+
+		@Override
+		public void run() {
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						setImage(iconNormal);
+					}
+				});
+			} catch (final Exception e) {
+				// nop
+			}
+		}
+	}
+
+	private class BlinkingRunnable extends AbstractFinishableRunnable {
+
+		/** temporary image used for blinking effect */
+		private Image tempImage;
+
+		@Override
+		public void run() {
+
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						tempImage = getImage();
+					}
+				});
+
+				while (!isFinished()) {
+
+					SwingUtilities.invokeAndWait(new Runnable() {
+						@Override
+						public void run() {
+							setImage(iconWorking);
+						}
+					});
+
+					Thread.sleep(BLINK_INTERVAL_MILLISECONDS);
+
+					SwingUtilities.invokeAndWait(new Runnable() {
+						@Override
+						public void run() {
+							setImage(tempImage);
+						}
+					});
+
+					Thread.sleep(BLINK_INTERVAL_MILLISECONDS);
+				}
+			} catch (final Exception e) {
+				/* ignore */
+			}
+
+		}
+	}
+}
