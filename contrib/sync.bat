f75fa006a5eb45caaf40c2c6632833601ffc25c3
Frank Jakop <frank.jakop@tolina.de>
2013-05-17 12:22:41 +0200

    fixed PATH / classpath issues on Windows

--- contrib/sync.bat
+++ contrib/sync.bat
@@ -1,10 +1,10 @@
 @echo off
 
 setlocal
-FOR /F "tokens=*" %%i in ('type %HOMEPATH%\.ngcalsync\env.properties') do SET %%i
+FOR /F "tokens=*" %%i in ('type %HOMEPATH%\.ngcalsync\env.properties') do SET "%%i"
 
 set PATH=%NOTES_HOME%;%PATH%
 set NOTES_JAR=%NOTES_HOME%\jvm\lib\ext\Notes.jar
 
-start javaw -cp *;%NOTES_JAR% de.jakop.ngcalsync.StartApplication
+start javaw -cp *;"%NOTES_JAR%" de.jakop.ngcalsync.StartApplication
 endlocal
